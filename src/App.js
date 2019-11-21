f9ce624ff7a5f470420aad82497690da5ec3ca84
AhasanNoor <megamind184@gmail.com>
2019-11-22 00:47:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    count: 1,
+    total: 35
+  }
+
+  handleDecrement = () => {
+    if(this.state.count === 1){
+      this.setState({
+        count: 1,
+        total: 35
+      })
+    }
+    else{
+      this.setState(prevState => ({
+        count: this.state.count - 1,
+        total: this.state.total - 35
+      }))
+    }
+  }
+
+  handleIncrement = () => {
+    this.setState(prevState => ({
+      count: this.state.count + 1,
+      total: this.state.total + 35
+    }))
+  }
+
+  render () {
+    return(
+      <div className="App">
+        <div className="App-body ">
+          <button onClick={this.handleDecrement}>-</button>
+          <h1 className="count">{this.state.count}</h1>
+          <button onClick={this.handleIncrement}>+</button>
+          <h1 className="total"><i className="fa fa-shopping-cart shop" aria-hidden="true"></i>  <i className="fa fa-usd doller" aria-hidden="true"></i>{this.state.total}</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
